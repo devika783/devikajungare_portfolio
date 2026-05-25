491d4b7a61240b23a40b4d8ebf83992c0a95342d
ADMIN <admin@example.com>
2026-05-25 16:17:16 +0530

    little allignment fix

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
         caseStudyBeyondWords: resolve(__dirname, 'case-study-beyond-words.html'),
         caseStudyJanAushadhi: resolve(__dirname, 'case-study-jan-aushadhi.html'),
         caseStudyMetroAccessibility: resolve(__dirname, 'case-study-metro-accessibility.html'),
+        caseStudyRemnants: resolve(__dirname, 'case-study-remnants.html'),
       },
     },
   },
